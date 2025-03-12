28c581c7a2b3ac8112a2c7219731c4e48fda9088
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-03-12 19:50:00 +0000

    save php 6

--- index.php
+++ index.php
@@ -14,14 +14,18 @@
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>The Area & Perimiter program, in PHP</title>
+  <title>The Area & Perimeter Program, in PHP</title>
   <style>
     .rectangle-title {
       color: white;
       font-size: 24px;
-      text-align: center;
+      text-align: left;
+      /* Align text to the left */
       font-family: Arial, sans-serif;
       margin-top: 20px;
+      /* Adjust margin as needed */
+      margin-left: 0.3px;
+      /* Optional: Add left margin for spacing */
     }
   </style>
 </head>
@@ -31,12 +35,12 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The Area & Perimiter program, in PHP</span>
+        <span class="mdl-layout-title">The Area & Perimeter Program, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/area_and_perimeter.png" alt="area and perimiter image" />
+        <img src="./images/area_and_perimeter.png" alt="Area and Perimeter Image" />
       </div>
       <div class="page-content-php">
         <?php
@@ -45,15 +49,22 @@
         echo "<h2 class='rectangle-title'>The dimensions of a rectangle are: $length cm and $width cm.</h2>";
         ?>
       </div>
+      <div class="page-content-php">
+        <?php echo "<h5 class='result'>The area is: </h5>";
+        echo "<h5 class='result'>The perimeter is: </h5>";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          $length = 5;
+          $width = 3;
+          $area = $length * $width;
+          $perimeter = 2 * ($length + $width);
+        }
+        ?>
+      </div>
       <form method="post">
         <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
           Calculate
         </button>
       </form>
-      <?php if ($_SERVER["REQUEST_METHOD"] == "POST") : ?>
-        <p>The area is: <?php echo number_format($area, 15); ?> cm²</p>
-        <p>The perimeter is: <?php echo number_format($perimeter, 16); ?> cm</p>
-      <?php endif; ?>
     </main>
   </div>
 </body>
